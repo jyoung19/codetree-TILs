d059115b108e71c213c8115b3076d596462247cd
jyoung19 <98319636+jyoung19@users.noreply.github.com>
2024-08-04 20:30:53 +0900

    Title: 배열의 평균, Time: 109ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240804/배열의 평균/ave-of-array.py
@@ -0,0 +1,23 @@
+arr_2d = [
+    list(map(int, input().split()))
+    for _ in range(2)
+]
+
+sum_rows = []
+sum_cols = []
+
+for i in range(2):
+    sum_row = 0
+    for j in range(4):
+        sum_row += arr_2d[i][j]
+    sum_rows.append(sum_row)
+
+for j in range(4):
+    sum_col = 0
+    for i in range(2):
+        sum_col += arr_2d[i][j]
+    sum_cols.append(sum_col)
+
+print(' '.join(str(sums/4) for sums in sum_rows))
+print(' '.join(str(sums/2) for sums in sum_cols))
+print(sum(sum_rows)/8)
