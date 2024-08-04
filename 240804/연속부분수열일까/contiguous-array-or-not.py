6682e0b2015f101dd4a26df402f7e88b3cdd6c7c
jyoung19 <98319636+jyoung19@users.noreply.github.com>
2024-08-04 20:06:59 +0900

    Title: 연속부분수열일까, Time: 109ms, Memory: 24MB, Status: Passed - CodeTree

--- 240804/연속부분수열일까/contiguous-array-or-not.py
+++ 240804/연속부분수열일까/contiguous-array-or-not.py
@@ -2,17 +2,20 @@ n1, n2 = map(int, input().split())
 A = list(map(int, input().split()))
 B = list(map(int, input().split()))
 
-idxs = list(filter(lambda x: A[x] == B[0], range(len(A))))
 flag = False
 
-if B[0] in A:
+if n2 <= n1:  # 수열 B가 수열 A보다 길 경우를 미리 체크
+    idxs = [i for i in range(n1 - n2 + 1) if A[i] == B[0]]  # A의 가능한 시작 인덱스 찾기
+
     for idx in idxs:
-        for i in range(len(B)):
-            if B[i] == A[idx + i]:
-                flag = True
-            else:
-                flag = False
+        match = True
+        for i in range(n2):
+            if A[idx + i] != B[i]:
+                match = False
                 break
+        if match:
+            flag = True
+            break  # 한 번이라도 일치하면 더 이상 확인할 필요 없음
 
 if flag == True:
     print("Yes")
