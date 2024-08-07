a70d528919010c82e8103f23c67f12099d7a6133
jyoung19 <98319636+jyoung19@users.noreply.github.com>
2024-08-07 21:49:26 +0900

    Title: 파스칼의 삼각형, Time: 145ms, Memory: 24MB, Status: Passed - CodeTree

--- 240807/파스칼의 삼각형/pascal's-triangle.py
+++ 240807/파스칼의 삼각형/pascal's-triangle.py
@@ -10,7 +10,7 @@ for j in range(n):
     arr_2d[j][0] = 1
     arr_2d[j][j] = 1
 
-# 2. 나머지 칸에 전부 숫자를 채웁니다.
+# 2. 나머지 칸에 전부 숫자를 채움
 for i in range(1, n):
     for j in range(n):
         arr_2d[i][j] = arr_2d[i - 1][j - 1] + arr_2d[i - 1][j]
@@ -18,8 +18,6 @@ for i in range(1, n):
 # 출력
 for row in arr_2d:
     for elem in row:
-        if elem == 0:
-            pass
-        else:
+        if elem != 0:
             print(elem, end=" ")
     print()
