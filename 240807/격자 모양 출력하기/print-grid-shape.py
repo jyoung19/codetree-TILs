0e82abf6a24cdd14e2daf83b55332bf1882e09b8
jyoung19 <98319636+jyoung19@users.noreply.github.com>
2024-08-07 22:12:49 +0900

    Title: 격자 모양 출력하기, Time: 124ms, Memory: 25MB, Status: Wrong Answer - CodeTree

--- 240807/격자 모양 출력하기/print-grid-shape.py
+++ 240807/격자 모양 출력하기/print-grid-shape.py
@@ -2,7 +2,7 @@ n, m = map(int, input().split())
 
 arr_2d = [[0 for _ in range(m)] for _ in range(m)]
 
-for i in range(m):
+for i in range(n):
     r, c = map(int, input().split())
     arr_2d[r-1][c-1] = r*c
 
