21737d7b07e5567bc2f96e842fb67d8eb5ed03b1
jyoung19 <98319636+jyoung19@users.noreply.github.com>
2024-08-11 13:49:55 +0900

    Title: 특정 문자의 유무, Time: 142ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240811/특정 문자의 유무/specific-character-presence.py
@@ -0,0 +1,13 @@
+arr1 = input()
+
+if ('ee' in arr1):
+    answer1 = 'Yes'
+else:
+    answer1 = 'No'
+
+if ('ab' in arr1):
+    answer2 = 'Yes'
+else:
+    answer2 = 'No'
+
+print(answer1, answer2)
