819bab4ec197e08c42fd05feff6edf21f0bde897
jyoung19 <98319636+jyoung19@users.noreply.github.com>
2024-08-23 22:40:54 +0900

    Title: 함수를 이용한 369 게임, Time: 176ms, Memory: 25MB, Status: Passed - CodeTree

--- 240823/함수를 이용한 369 게임/369-games-using-functions.py
+++ 240823/함수를 이용한 369 게임/369-games-using-functions.py
@@ -1,21 +1,19 @@
-def func(a, b):
-    cnt = 0
-    for i in range(a, b+1):
-         # 한 자리 수인 경우
-        if i < 10: 
-            if (i == 3) or (i == 6) or (i == 9):
-                cnt += 1
-        # 두 자리 수인 경우
-        else:
-            digit1 = i // 10  # 십의 자리 수
-            digit2 = i % 10  # 일의 자리 수
-            if (digit1 == 3) or (digit1 == 6) or (digit1 == 9):
-                cnt += 1
-            elif (digit2 == 3) or (digit2 == 6) or (digit2 == 9):
-                cnt += 1
-            elif i % 3 == 0:
-                cnt += 1
-    return cnt
+def count_special_numbers(a, b):
+    def is_special(num):
+        # 숫자에 3, 6, 9가 포함되어 있거나 3의 배수인지 확인
+        if '3' in str(num) or '6' in str(num) or '9' in str(num) or num % 3 == 0:
+            return True
+        return False
 
+    count = 0
+    for num in range(a, b + 1):
+        if is_special(num):
+            count += 1
+
+    return count
+
+# 입력 받기
 a, b = map(int, input().split())
-print(func(a, b))
+
+# 결과 출력
+print(count_special_numbers(a, b))
