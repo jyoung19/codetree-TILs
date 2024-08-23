dbc677240e9908ba8fb40cb10090023e30bde204
jyoung19 <98319636+jyoung19@users.noreply.github.com>
2024-08-23 22:56:24 +0900

    Title: 함수를 이용한 윤년 판별, Time: 108ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240823/함수를 이용한 윤년 판별/tell-the-function-using-a-leap-year.py
+++ 240823/함수를 이용한 윤년 판별/tell-the-function-using-a-leap-year.py
@@ -1,8 +1,8 @@
 def func(n):
-    if n % 4 == 0:
-        return "true"
-    elif (n % 100 == 0) and (n % 400 != 0):
+    if (n % 4 == 0) and (n % 100 == 0) and (n % 400 != 0):
         return "false"
+    elif (n % 100 != 0) or (n % 400 == 0):
+        return "true"
     else:
         return "false"
     
